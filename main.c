40f027ab70f6bdb131344753fcde73c2188e04a0
osmancevik <github.fencing841@passmail.net>
2026-01-24 09:01:07 +0300

    1.5 solved

--- main.c
+++ main.c
@@ -2,17 +2,5 @@
 
 int main()
 {
-    int fahr, celsius;
-    int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = upper;
-    while (fahr >= lower) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%3d %6d\n", fahr, celsius);
-        fahr = fahr - step;
-    }
 }

6b2c5aa9029eac020d7795f3ed023744163759b9
osmancevik <github.fencing841@passmail.net>
2026-05-05 07:49:24 +0300

    .

---
+++ Chapter-1/1.12.c
@@ -0,0 +1,15 @@
+// Exercise 1-12. Write a program that prints its input one word per line.
+
+#include <stdio.h>
+
+int main() {
+
+  int c;
+  while ((c = getchar()) != EOF) {
+
+    if (c == ' ' || c == '\n' || c == '\t') {
+      putchar('\n');
+    } else
+      putchar(c);
+  }
+}
